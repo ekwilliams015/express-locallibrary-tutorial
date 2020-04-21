2cbcf98f5f142047c6d807c7b11458f141b5bcb8
Elijah Williams <ekwilliams015@gmail.com>
2020-04-20 19:11:35 -0500

    add models folder

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = mongodb+srv://Elijah:<Kevin015>@cluster0-nt5my.mongodb.net/local_library?retryWrites=true&w=majority;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
