90fd8f676f7b93a01adda68a2a0bf8f32907cd6a
Elijah Williams <ekwilliams015@gmail.com>
2020-04-28 18:34:37 -0500

    ADD FLAIR

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site 
 
-var app = express();
+const app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = mongodb + srv://Elijah:<Kevin015>@cluster0-nt5my.mongodb.net/local_library?retryWrites=true;
+const mongoDB = mongodb + srv: Elijah:<Kevin015>@cluster0-nt5my.mongodb.net/local_library?retryWrites=true;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); //Add catalog routes to middleware chain
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
